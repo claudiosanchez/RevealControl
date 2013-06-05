d5254a1d958cdfcf7ec8877e3c9d11ceee9cfd7a
Claudio Sanchez <claudio@megsoftconsulting.com>
2013-06-05 16:32:32 -0400

    some more translating

--- SWRevealViewController.cs
+++ SWRevealViewController.cs
@@ -10,10 +10,35 @@
 using System;
 using MonoTouch.UIKit;
 using System.Drawing;
+using RevealControl;
 
 
-public class SWRevealViewController : UIViewController, UIGestureRecognizerDelegate 
+public class SWRevealViewController : UIViewController
 {
+	public float RearViewRevealWidth { get; set;}
+	public float RearViewRevealOverdraw { get; set;}
+
+	public float RightViewRevealWidth { get; set;}
+	public float RightViewRevealOverdraw { get; set;}
+
+
+	public FrontViewPosition FrontViewPosition {
+		get;
+		set;
+	}
+
+	public float FrontViewShadowRadius {
+		get;
+		set;
+	}
+
+	public SizeF FrontViewShadowOffset {
+		get;
+		set;
+	}
+
+	private UIGestureRecognizerDelegate _delegate;
+
 	public UIViewController RearViewController {
 		get;
 		set;
@@ -34,46 +59,145 @@ public class SWRevealViewController : UIViewController, UIGestureRecognizerDeleg
 		set;
 	}
 
+	FrontViewPosition _panInitialFrontPosition;
+
+	const int FrontViewPositionNone = 0xff;
+
+	FrontViewPosition RearViewPosition {
+		get;
+		set;
+	}
+
+	FrontViewPosition RightViewPosition {
+		get;
+		set;
+	}
+
+	bool BounceBackOnOverdraw {
+		get;
+		set;
+	}
+
+	bool BounceBackOnLeftOverdraw {
+		get;
+		set;
+	}
+
+	bool StableDragOnOverdraw {
+		get;
+		set;
+	}
+
+	bool StableDragOnLeftOverdraw {
+		get;
+		set;
+	}
+
+	float _quickFlickVelocity;
+
+	float QuickFlickVelocity {
+		get;
+		set;
+	}
+
+	double ToggleAnimationDuration {
+		get;
+		set;
+	}
+
+	bool UserInteractionStore {
+		get;
+		set;
+	}
+
+	void InitializeDefaultProperties ()
+	{
+		RearViewPosition  = FrontViewPosition.FrontViewPositionLeft;
+		RightViewPosition = FrontViewPosition.FrontViewPositionLeft;
+		RearViewRevealWidth = 260.0f;
+		RearViewRevealOverdraw = 60.0f;
+		RightViewRevealWidth = 260.0f;
+		RightViewRevealOverdraw = 60.0f;
+		BounceBackOnOverdraw = true;
+		BounceBackOnLeftOverdraw = true;
+		StableDragOnOverdraw = false;
+		StableDragOnLeftOverdraw = false;
+		QuickFlickVelocity = 250.0f;
+		ToggleAnimationDuration = 0.25;
+		FrontViewShadowRadius = 2.5f;
+		FrontViewShadowOffset = new SizeF(0.0f, 2.5f);
+		UserInteractionStore = true;
+	}
+
 	void Initialize ()
 	{
-		throw new NotImplementedException ();
+		InitializeDefaultProperties();
+	}
+
+	public SWRevealViewController ()
+	{
+		
 	}
 
 	public SWRevealViewController (UIViewController rearViewController, UIViewController frontViewController)
 	{
+		Initialize ();
 
 		RearViewController = rearViewController;
 		FrontViewController = frontViewController;
 
-		Initialize ();
+
 	}
 
 	public void SetFrontViewController (UIViewController frontController, bool isAnimated)
 	{
-
+		throw new NotImplementedException ();
 	}
 
 	public void RevealToggleAnimated (bool isAnimated)
 	{
-
+		throw new NotImplementedException ();
 	}
 
 	public void RightRevealToggle (bool iSAnimated)
 	{
+		throw new NotImplementedException ();
 	}
 
-
-	void GetRevealWidth(float revealWidth, float revealOverdraw, int symetry)
+	public void GetBounceBack(out bool bounceBack, out bool stableDrag, int simetry)
 	{
+		throw new NotImplementedException ();
 	}
 
-	void GetBounceBack(bool bounceBack, bool stableDrag, int simetry)
+	public void GetAdjustedFrontViewPosition(FrontViewPosition position, int simetry)
 	{
+		throw new NotImplementedException ();
 	}
 
-	void GetAdjustedFrontViewPosition(FrontViewPosition position, int simetry)
+	public void GetRevealWidth (out float revealWidth, out float revealOverdraw, int simetry)
 	{
+		throw new NotImplementedException ();
 	}
+
+	public override void LoadView ()
+	{
+		
+		// This is what Apple tells us to set as the initial frame, which is of course totally irrelevant
+		// with the modern view controller containment patterns, let's leave it for the sake of it!
+		var frame = UIScreen.MainScreen.ApplicationFrame;
+
+		// create a custom content view for the controller
+		var ContentView = new SWRevealView (frame, this);
+
+		// set the content view to resize along with its superview
+		//[_contentView setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
+
+		// set our contentView to the controllers view
+		//self.view = _contentView;
+
+	}
+
+
 }
 
 
--- View.cs
+++ View.cs
@@ -10,45 +10,12 @@
 using System;
 using MonoTouch.UIKit;
 using System.Drawing;
+using System.Linq;
 
 namespace RevealControl
 {
-	/*
-@property (nonatomic, readonly) UIView *rearView;
-@property (nonatomic, readonly) UIView *rightView;
-@property (nonatomic, readonly) UIView *frontView;
-@property (nonatomic, assign) BOOL disableLayout;
-	  * */
-
-
 	public class SWRevealView: UIView
 	{
-		void Initialize ()
-		{
-			FrontView = new UIView (Bounds);
-			FrontView.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
-
-			AddSubview (FrontView);
-
-			var layer = FrontView.Layer;
-
-			layer.MasksToBounds = false;
-			layer.ShadowColor = UIColor.Black.CGColor;
-			layer.ShadowOpacity = 1.0f;
-			/*
-			 * frontViewLayer.shadowOffset = _c.frontViewShadowOffset;
-        frontViewLayer.shadowRadius = _c.frontViewShadowRadius
-        */
-
-		}
-
-		public SWRevealView (RectangleF frame, SWRevealViewController controller): base(frame)
-		{
-			Controller = controller;
-			Initialize ();
-
-		}
-
 		public UIView RearView {
 			get;
 			private set;
@@ -75,7 +42,157 @@ namespace RevealControl
 			private set;
 		}
 
+		public SWRevealView (RectangleF frame, SWRevealViewController controller): base(frame)
+		{
+			Controller = controller;
+			Initialize ();
+		}
 
+		void Initialize ()
+		{
+			FrontView = new UIView (Bounds);
+			FrontView.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
+
+			AddSubview (FrontView);
+
+			var layer = FrontView.Layer;
+
+			layer.MasksToBounds = false;
+			layer.ShadowColor = UIColor.Black.CGColor;
+			layer.ShadowOpacity = 1.0f;
+
+			layer.ShadowOffset = Controller.FrontViewShadowOffset;
+			layer.ShadowRadius = Controller.FrontViewShadowRadius;
+		}
+
+		private void LayoutRearViews ()
+		{
+			float rearWidth = Controller.RearViewRevealWidth + Controller.RearViewRevealOverdraw;
+			RearView.Frame = new RectangleF (0, 0, rearWidth, Bounds.Size.Height);
+
+			var rightWidth = Controller.RightViewRevealWidth + Controller.RightViewRevealOverdraw;
+			RightView.Frame = new RectangleF (Bounds.Size.Width- rightWidth, 0, rightWidth, Bounds.Size.Height);
+		}
+
+		public override void LayoutSubviews ()
+		{
+			if (DisableLayout)
+				return;
+
+			this.LayoutRearViews ();
+			float xPosition = FrontLocationForPosition (Controller.FrontViewPosition);
+			FrontView.Frame = new RectangleF (xPosition, 0.0f, Bounds.Size.Width, Bounds.Size.Height);
+
+			UIBezierPath shadowPath = UIBezierPath.FromRect (FrontView.Bounds);
+			FrontView.Layer.ShadowPath = shadowPath.CGPath;
+
+		}
+
+		void PrepareForNewPosition (FrontViewPosition newPosition)
+		{
+			if (RearView == null || RightView == null) return;
+
+			int symetry = newPosition < FrontViewPosition.FrontViewPositionLeft ? -1 : 1;
+
+			var rearIndex = Subviews.ToList ().IndexOf (RearView);
+			var rightIndex = Subviews.ToList ().IndexOf (RightView);
+
+			if ((symetry < 0 && rightIndex < rearIndex) || (symetry > 0 && rearIndex < rightIndex))
+				ExchangeSubview (rightIndex, rearIndex);
+		}
+
+		void PrepareRearViewForPosition(FrontViewPosition newPosition)
+		{
+			if ( RearView == null )
+			{
+				RearView = new UIView (Bounds);
+				RearView.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
+				InsertSubviewBelow (RearView, FrontView);
+			}
+			LayoutRearViews ();
+			PrepareForNewPosition (newPosition);
+		}
+
+		FrontViewPosition _frontViewPosition;
+
+		float FrontLocationForPosition(FrontViewPosition newPosition)
+		{
+			var location = 0.0f;
+
+			float revealWidth, revealOverdraw;
+			int simetry = (_frontViewPosition < FrontViewPosition.FrontViewPositionLeft) ? -1 : 1;
+
+			Controller.GetRevealWidth (out revealWidth, out revealOverdraw, simetry);
+
+			if (_frontViewPosition == FrontViewPosition.FrontViewPositionRight)
+				location = revealWidth;
+			else if (_frontViewPosition > FrontViewPosition.FrontViewPositionRight)
+				location = revealWidth + revealOverdraw;
+
+			return location * simetry;
+
+
+
+		}
+
+		void DragFrontViewToXPosition(float xPosition)
+		{
+			xPosition = AdjustedDragLocationForLocation (xPosition);
+			FrontView.Frame = new RectangleF (xPosition, 0.0f, Bounds.Size.Width, Bounds.Size.Height);
+		}
+
+		void PrepareRightViewForPosition(FrontViewPosition newPosition)
+		{
+			if (RightView == null) {
+				RightView = new UIView (Bounds);
+				RightView.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
+				InsertSubviewBelow (RightView, FrontView);
+			}
+
+			LayoutRearViews ();
+			PrepareForNewPosition (newPosition);
+		
+		}
+
+		private void GetAdjustedFrontViewPosition(FrontViewPosition frontViewPosition, int symetry)
+		{
+				
+		}
+
+		private float AdjustedDragLocationForLocation(float x)
+		{
+			float result, revealWidth, revealOverdraw;
+			bool bounceBack, stableDrag;
+			var position = Controller.FrontViewPosition;
+
+			int symetry = x<0 ? -1 : 1;
+
+			Controller.GetRevealWidth (out revealWidth, out revealOverdraw, symetry);
+
+			Controller.GetBounceBack (out bounceBack, out stableDrag, symetry);
+
+			bool stableTrack = (!bounceBack || stableDrag || (position == FrontViewPosition.FrontViewPositionRightMost) || position==FrontViewPosition.FrontViewPositionLeftSideMost);
+
+			if (stableTrack) {
+				revealWidth += revealOverdraw;
+				revealOverdraw = 0.0f;
+			}
+
+			x = x * symetry;
+	
+
+		if (x <= revealWidth)
+			result = x;         // Translate linearly.
+
+		else if (x <= revealWidth+2*revealOverdraw)
+			result = revealWidth + (x-revealWidth)/2;   // slow down translation by halph the movement.
+
+		else
+			result = revealWidth+revealOverdraw;        // keep at the rightMost location.
+
+		return result * symetry;
+	
+		}
 	}
 }
 
